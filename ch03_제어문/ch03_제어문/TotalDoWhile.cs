737be7b23bb810cab8cab693e103da0b9a44e0d3
123ekrxjtj <123ekrxjtj@naver.com>
2022-09-26 13:03:40 +0900

    Total 완성

--- ch03_제어문/ch03_제어문/TotalDoWhile.cs
+++ ch03_제어문/ch03_제어문/TotalDoWhile.cs
@@ -21,14 +21,11 @@ public class TotalDoWhile
                 holSum += num;
             }
 
-            Console.Write("하나의 정수 입력 ( -1 ~ 10 사이 ) : ");
-            num = Convert.ToInt32(Console.ReadLine());
-
-            while (num > 10 || num < -1)
+            do
             {
                 Console.Write("하나의 정수 입력 ( -1 ~ 10 사이 ) : ");
                 num = Convert.ToInt32(Console.ReadLine());
-            };
+            } while (num > 10 || num < -1) ;
         } while (num!=-1);
 
         Console.WriteLine("입력 데이터 갯수 : {0}",count);
