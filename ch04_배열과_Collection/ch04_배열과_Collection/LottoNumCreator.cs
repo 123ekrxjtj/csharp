2cfbd885e3ed2eb050b2e00f114665e72f874a0f
SH501 <123ekrxjtj@naver.com>
2022-09-30 14:38:07 +0900

    09/30 로또생성기 + Sort

---
+++ ch04_배열과_Collection/ch04_배열과_Collection/LottoNumCreator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class LottoNumCreator
+{
+    public static void sort(int[] a)
+    {
+        for (int i = 0; i < a.Length - 1; i++)
+        {
+            for (int j = i + 1; j < a.Length; j++)
+            {
+                int tmp;
+                if (a[i] < a[j])
+                {
+                    tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
+        int[] nums = new int[6];
+        Console.WriteLine("로또 번호 생성기");
+        int tmpN;
+            
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                tmpN = 0;
+                int k = 0;
+
+                while (k < 6)
+                {
+                    if (tmpN != nums[k]) k++;
+                    else
+                    {
+                        tmpN = random.Next();
+                        tmpN = 1 + tmpN % 45;
+                        k = 0;
+                    }
+                }
+                nums[j] = tmpN;
+            }
+
+            sort(nums);
+
+            for (int m = 0; m < 6; m++)
+            {
+                Console.Write("{0}\t", nums[m]);
+                nums[m] = 0;
+            }
+            Console.WriteLine();
+        }
+    }        
+}
---
+++ ch04_배열과_Collection/ch04_배열과_Collection/Sort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch04_배열과_Collection
+{
+    public class Sort
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = new int[4] { 1, 3, 2, 4 };
+            Console.WriteLine("정렬 전 데이터");
+
+            for (int i = 0; i < a.Length-1; i++)
+            {
+                for (int j = i+1; j < a.Length; j++)
+                {
+                    int tmp;
+                    if(a[i] < a[j])
+                    {
+                        tmp = a[i];
+                        a[i] = a[j];    
+                        a[j] = tmp; 
+                    }
+                }
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
