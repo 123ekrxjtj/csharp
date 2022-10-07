2b42f1e9d74fb04946013c88b823df8b3584ef20
SH501 <123ekrxjtj@naver.com>
2022-10-07 14:33:57 +0900

    1007 6장 중간

---
+++ ch05/ch05/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Shape
+{
+    public class Rectangle
+    {
+        public int width;
+        public int height;
+
+        public int round()
+        {
+            int round = 2 * (width + height);
+            return round;
+        }
+
+        public double areaHalf()
+        {
+            double areaHalf = width * height / (double)2;
+            return areaHalf;
+        }
+
+        public bool isSquare()
+        {
+            bool isSquare;
+            if(width == height) isSquare = true;
+            else isSquare = false;
+
+            return isSquare;
+        }
+    }
+}
---
+++ ch05/ch05/ch5_2_Method.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Shape;
+
+public class ch5_2_Method
+{
+    public static void Main(string[] args)
+    {
+        Rectangle rect = new Rectangle();
+        Console.Write("사각형의 가로의 길이를 입력하세요 : ");
+        rect.width = Convert.ToInt32(Console.ReadLine());
+        Console.Write("사각형의 세로의 길이를 입력하세요 : ");
+        rect.height = Convert.ToInt32(Console.ReadLine());
+
+        int round = rect.round();
+        double areaHalf = rect.areaHalf();
+
+        Console.WriteLine("사각형의 둘레의 길이는 {0}이고",round);
+        Console.WriteLine("사각형의 면적의 반은 {0}이고",areaHalf);
+
+        if (rect.isSquare()) Console.WriteLine("정사각형이다");
+        else Console.WriteLine("직각사각형이다");
+    }
+}
