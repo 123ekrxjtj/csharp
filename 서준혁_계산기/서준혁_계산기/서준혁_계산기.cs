e74b4f311ac226ff1e82ef11b27153b1104b48bc
SH408 <SH408@DESKTOP-8O9KL0Q>
2022-11-17 13:13:18 +0900

    계산기 진행중

--- 서준혁_계산기/서준혁_계산기/서준혁_계산기.cs
+++ 서준혁_계산기/서준혁_계산기/서준혁_계산기.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;  // 자료구조 관련 클래스 추가
+
 
 namespace 서준혁_계산기
 {
     public partial class 서준혁_계산기 : Form
     {
+        //  계산기 연산 시 쓸 스택들
+        Stack num_Stack;    //  숫자 집어넣을 스택
+        Stack oper_Stack;   //  연산자 집어넣을 스택
+
         // 계산기에서 사용할  전역 변수 선언
         double num1, num2; // 두 수 저장할 변수  
         string buff = "";         // 임시 보관 할 변수
@@ -24,6 +30,9 @@ namespace 서준혁_계산기
         public 서준혁_계산기()
         {
             InitializeComponent();
+
+            num_Stack = new Stack();
+            oper_Stack = new Stack();
         }
 
 
@@ -50,12 +59,14 @@ namespace 서준혁_계산기
         #region : 클리어 클릭시 동작
         private void Bt_clear_Click(object sender, MouseEventArgs e)
         {
+            checkNum = 0;
             tx_Result.Text = "";
             num1 = 0;
             num2 = 0;
             buff = "";
             output = "";
             result = 0.0;
+            oper = ' '; 
         }
         #endregion
 
@@ -63,12 +74,15 @@ namespace 서준혁_계산기
         private void Bt_oper_Click(object sender, MouseEventArgs e)
         {
             Button btn = (Button)sender;
+
+
+
             //  처음 계산 시
             if (checkNum == 0)
             {
                 num1 = Convert.ToSingle(buff); // 3
             }
-
+            
             buff = "";                     // 0
             output += btn.Text;            // 3 + 
             tx_Result.Text = output;        // 3 + 
@@ -111,7 +125,8 @@ namespace 서준혁_계산기
             num1 = Convert.ToDouble(result);
             checkNum = 1;
             //  btn에는 = 가 저장됨
-            output += btn.Text + result.ToString();   //  =  결과값 
+            output += "=" + result.ToString();   //  =  결과값 
+                
             tx_Result.Text = output;
             // num2=8이고 result=3+8 btn(=)11,  
             // output=3+8 btn(=)11,Textbox1=3+8=11
